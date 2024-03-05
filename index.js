fae10450550bcb3ac647901c02456bc06406b843
Mawj_Aljadirjy <eng_mawj_aljadirjy@yahoo.com>
2024-03-05 20:48:56 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// Function to divide two numbers
+const divide = (a, b) => a / b;
+
+// Arrow function to square a number
+const square = x => x * x;
+
+// Arrow function to add two numbers
+const add = (a, b) => a + b;
+
+module.exports = { divide, square, add };
